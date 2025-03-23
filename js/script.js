1adb8923a76b87964c9c96e0573ce9f486d6ea3c
Marco Antonio Guerrero Asis <marcogasis01@gmail.com>
2025-03-23 05:20:19 -0600

    Se agrega el getelement para alumno y curso

--- js/script.js
+++ js/script.js
@@ -31,7 +31,62 @@ fetch(`http://localhost:3000/api/certificados/${alumnoId}`)
   .then(response => response.json())
   .then(data => {
     console.log(data); // Muestra los datos obtenidos
-  })
+    console.log(data.materias[0].nombre_materia);
+
+    if (!data.alumno) {
+        console.error("No se encontraron datos del alumno");
+        return;
+    }
+
+    const fechaIniFormateada = new Date(data.alumno.fecha_inicio).toLocaleDateString("es-MX", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+      });
+    
+    const fechaFinFormateada = new Date(data.alumno.fecha_fin).toLocaleDateString("es-MX", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+      });
+
+    //Asignamos valores a tabla del alumno
+
+    document.getElementById("nombre-alumno").textContent = data.alumno.nombre_alumno;
+    document.getElementById("calificacion-alumno").textContent = data.alumno.cali;
+    document.getElementById("grupo-alumno").textContent = data.alumno.clave_grupo;
+    document.getElementById("curso-alumno").textContent = data.alumno.clave_curso;
+    document.getElementById("inicio-alumno").textContent = fechaIniFormateada;
+    document.getElementById("fin-alumno").textContent = fechaFinFormateada;
+
+
+    if (!data.materias || data.materias.length === 0) {
+        console.log("No hay materias disponibles");
+        return;
+    }
+    const tablaBody = document.getElementById("tabla-body");
+    if (!tablaBody) {
+        console.error("Error: No se encontro una tabla con id tabla-body");
+        return;
+    }
+
+    tablaBody.innerHTML = "";
+
+    data.materias.forEach(materia => {
+        const fila = document.createElement("tr");
+        fila.innerHTML = `
+        <td>${materia.nombre_materia}</td>
+        <td>${materia.duracion_t}</td>
+        `
+        tablaBody.appendChild(fila);
+    });
+
+
+  }
+
+    )
+ 
+
   .catch(error => {
     console.error('Error al obtener los datos', error); // Muestra un error si la solicitud falla
   });
