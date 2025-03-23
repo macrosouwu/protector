5fbefd8c6be1a5ef5dc649c12fe32ff9101e2d29
Marco Antonio Guerrero Asis <marcogasis01@gmail.com>
2025-03-23 03:34:55 -0600

    ya funciona el fetch, falta renderizar en el html

--- js/script.js
+++ js/script.js
@@ -20,8 +20,20 @@ inputs.forEach(input => {
 });
 
 // Empezamos con la API
-const alumnoId = 1;
+// Obtener el ID desde los parámetros de consulta
 
-fetch(`http://localhost:3000/api/alumno/${alumnoId}`)
-.then(response => response.json())
-.catch(error => console.error('Error al obtener los datos', error));
+const urlParams = new URLSearchParams(window.location.search);
+const alumnoId = urlParams.get('id'); // Obtiene el valor del parámetro "id"
+
+console.log(alumnoId); // Verás "1" si el parámetro id es 1
+
+fetch(`http://localhost:3000/api/certificados/${alumnoId}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data); // Muestra los datos obtenidos
+  })
+  .catch(error => {
+    console.error('Error al obtener los datos', error); // Muestra un error si la solicitud falla
+  });
+
+
