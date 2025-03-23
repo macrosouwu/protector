87a1be5edd1dc2d4867c096fb9a2c46f77744f07
Marco Antonio Guerrero Asis <marcogasis01@gmail.com>
2025-03-23 02:42:25 -0600

    Se empezo a trabajar con la api, añadiendo el Fetch

--- js/script.js
+++ js/script.js
@@ -18,3 +18,26 @@ inputs.forEach(input => {
     input.addEventListener("focus", focusFunc);
     input.addEventListener("blur", blurFunc);    
 });
+
+// Empezamos con la API
+const urlParams = new URLSearchParams(window.location.search);
+const alumnoId = window.location.pathname.split('/').pop(); // se obtiene el id desde el url
+
+// solicitud http usando fetch
+fetch(`http://localhost:3000/api/alumno/${alumnoId}`)
+.then(response => {
+    if (!response.ok) {
+        throw new Error("Error al obtener los datos del alumno");        
+    }
+    return response.json(); // convierte respuesta a formato JSON
+})
+.then(data => {
+    //aqui trabajamos con los datos obtenidos
+    console.log(data);
+    
+    // mostrar los datos en la pagina
+    const alumnoDiv = document.getElementById('alumno');
+    alumnoDiv.innerHTML = `
+    
+    `    
+})
